65ef7ff189ddf7aa6ec4428530b93a0ba5541a91
David <kcufow@gmail.com>
2017-05-16 19:01:13 +0800

    完成遍历省市三级列表的功能

---
+++ app/src/main/java/com/coolweather/android/Constant.java
@@ -0,0 +1,8 @@
+package com.coolweather.android;
+
+
+
+public interface Constant {
+     String  provinceaddress="http://guolin.tech/api/china";
+     String  baseUrl="http://guolin.tech/api/china/";
+}
---
+++ app/src/main/java/com/coolweather/android/util/Httputil.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.util;
+
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class Httputil {
+    public static void sendOkHttpREquest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+
+
+    }
+
+
+}
